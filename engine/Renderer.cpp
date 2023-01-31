31488748b44d4c25121ac44d53379532815c7102
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2023-01-31 22:58:00 +0200

    Added more places of animationVisitor

--- engine/Renderer.cpp
+++ engine/Renderer.cpp
@@ -32,6 +32,7 @@ void Renderer::RenderViewport(Viewport* viewport, Visitor* visitor)
 
     viewport->Bind();
     visitor->Run(viewport->scene.get(), viewport->camera.get());
+    animationVisitor.Run(viewport->scene.get(), viewport->camera.get());
 }
 
 void Renderer::RenderViewportAtPos(int x, int y, Visitor* visitor)
@@ -45,6 +46,7 @@ void Renderer::RenderViewportAtPos(int x, int y, Visitor* visitor)
 void Renderer::RenderAllViewports()
 {
     defaultVisitor.Init();
+    animationVisitor.Init();
 //    viewports[0]->scene->nextCyclicDescentStep();
 
     for (auto& viewport: viewports)
