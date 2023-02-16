aac18bd23f9905dc70502e9962888a3e10d8597d
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2023-02-16 17:26:01 +0200

    Added different shaders for different entities

--- tutorial/Assignment4/BasicScene.cpp
+++ tutorial/Assignment4/BasicScene.cpp
@@ -99,11 +99,30 @@ void BasicScene::BuildImGui()
 void BasicScene::Update(const Program& program, const Eigen::Matrix4f& proj, const Eigen::Matrix4f& view, const Eigen::Matrix4f& model)
 {
     Scene::Update(program, proj, view, model);
-//    program.SetUniform4f("lightColor", 0.8f, 0.3f, 0.0f, 0.5f);
-//    program.SetUniform4f("Kai", 1.0f, 0.3f, 0.6f, 1.0f);
-//    program.SetUniform4f("Kdi", 0.5f, 0.5f, 0.0f, 1.0f);
-//    program.SetUniform1f("specular_exponent", 5.0);
-//    program.SetUniform4f("light_position", 0.0, 15.0, 0.0, 1.0);
+    if (std::strcmp(program.name.c_str(), "itemShader") == 0)
+    {
+        gameplay.bonusShader->SetUniform4f("lightColor", 0.0f, 0.392f, 0.0f, 1.0f);
+        gameplay.bonusShader->SetUniform4f("Kai", 0.0f, 0.392f, 0.0f, 0.4f);
+        gameplay.bonusShader->SetUniform4f("Kdi", 0.0f, 0.392f, 0.0f, 0.4f);
+        gameplay.bonusShader->SetUniform1f("specular_exponent", 5.0);
+        gameplay.bonusShader->SetUniform4f("light_position", 0.0, 15.0, 0.0, 1.0);
+    }
+    if (std::strcmp(program.name.c_str(), "enemyShader") == 0)
+    {
+        gameplay.itemShader->SetUniform4f("lightColor", 0.545f, 0.0f, 0.0f, 1.0f);
+        gameplay.itemShader->SetUniform4f("Kai", 0.545f, 0.0f, 0.0f, 0.4f);
+        gameplay.itemShader->SetUniform4f("Kdi", 0.545f, 0.0f, 0.0f, 0.4f);
+        gameplay.itemShader->SetUniform1f("specular_exponent", 3.0);
+        gameplay.itemShader->SetUniform4f("light_position", 0.0, 15.0, 0.0, 1.0);
+    }
+    if (std::strcmp(program.name.c_str(), "bonusShader") == 0)
+    {
+        gameplay.enemyShader->SetUniform4f("lightColor", 1.0f, 0.843f, 0.0f, 1.0f);
+        gameplay.enemyShader->SetUniform4f("Kai", 1.0f, 0.843f, 0.0f, 0.4f);
+        gameplay.enemyShader->SetUniform4f("Kdi", 1.0f, 0.843f, 0.0f, 0.4f);
+        gameplay.enemyShader->SetUniform1f("specular_exponent", 3.0);
+        gameplay.enemyShader->SetUniform4f("light_position", 0.0, 15.0, 0.0, 1.0);
+    }
 }
 
 void BasicScene::updateGameplay()
